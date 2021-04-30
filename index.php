448aefcce595d743b3b2551a69a8fd416beaee1f
korkem <73012254+kkorkemm@users.noreply.github.com>
2021-04-30 22:38:41 +0600

    лаб25

--- index.php
+++ index.php
@@ -3,13 +3,37 @@
   require_once('inc/lib.inc.php');
   require_once('inc/data.inc.php');
 
+
+  // Инициализация заголовков страницы
+  $title = 'Сайт нашей школы';
+  $header = "$welcome, Гость!";
+  $id = strtolower(strip_tags(trim($_GET['id'])));
+  switch($id){
+    case 'about':
+      $title = 'О сайте';
+      $header = 'О нашем сайте';
+      break;
+    case 'contact':
+      $title = 'Контакты';
+      $header = 'Обратная связь';
+      break;
+    case 'table':
+      $title = 'Таблица умножения';
+      $header = 'Таблица умножения';
+      break;
+    case 'calc':
+      $title = 'Он-лайн калькулятор';
+      $header = 'Калькулятор';
+      break;
+  }
+
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-  <title>Сайт нашей школы</title>
+  <title> <?php echo $title?> </title>
   <meta charset="utf-8" />
   <link rel="stylesheet" href="style.css" />
 </head>
@@ -22,9 +46,26 @@
 
   <div id="content">
     <!-- Заголовок -->
-    <h1><?= $welcome ?>, Гость!</h1>
+    <h1><?php echo $header?></h1>
     <!-- Заголовок -->
-    <?php require_once('inc/index.inc.php') ?>
+    <?php
+      switch($id){
+        case 'about':
+          include 'about.php';
+          break;
+        case 'contact':
+          include 'contact.php';
+          break;
+        case 'table':
+          include 'table.php';
+          break;
+        case 'calc':
+          include 'calc.php';
+          break;
+        default:
+          include 'inc/index.inc.php';
+      }
+    ?>
   </div>
   <div id="nav">
     <?php require_once('inc/menu.inc.php') ?>
